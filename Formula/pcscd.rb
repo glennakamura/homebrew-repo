e9e08ce3584047fdc698f2d8856d3b38e777ff5a
Glen Nakamura <glen@imodulo.com>
2018-03-14 21:27:26 -1000

    pcscd winscard-homebrew (install only pcscd)

--- Formula/pcscd.rb
+++ Formula/pcscd.rb
@@ -1,15 +1,14 @@
 class Pcscd < Formula
   desc "Middleware to access a smart card using SCard API"
   homepage "https://pcsclite.alioth.debian.org"
-  url "https://github.com/glennakamura/PCSC.git", :using => :git, :revision => "61fd3364737a03dde0d5b7adffb377a258584544"
-  version "winscard-beta"
+  url "https://github.com/glennakamura/PCSC.git", :using => :git, :revision => "39a0650c32bb849b6520b77080e54393379a0b4c"
+  version "winscard-homebrew"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool"  => :build
 
   def install
-    system "sed", "-i", "", "s|^sbin_PROGRAMS|bin_PROGRAMS|", "./src/Makefile.am"
     system "./bootstrap"
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
