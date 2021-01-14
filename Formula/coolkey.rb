b1d2383c28ef336b3055ea2721e500c98946d686
Glen Nakamura <glen@imodulo.com>
2021-01-14 01:44:17 -1000

    coolkey homebrew-1.1.0-40b (fix log naming conflict)

--- Formula/coolkey.rb
+++ Formula/coolkey.rb
@@ -2,8 +2,8 @@ class Coolkey < Formula
   desc "PKCS#11 Module for CoolKey and CAC products"
   homepage "https://www.dogtagpki.org/wiki/CoolKey"
   url "https://github.com/glennakamura/coolkey.git",
-      :tag      => "homebrew-1.1.0-40a",
-      :revision => "7b97433df3a20014f65617eecc6815e5f819d2a3"
+      :tag      => "homebrew-1.1.0-40b",
+      :revision => "f9e8098e46e4d4f70de516b17d1c54cfdb8b18f8"
 
   depends_on "autoconf"   => :build
   depends_on "automake"   => :build
