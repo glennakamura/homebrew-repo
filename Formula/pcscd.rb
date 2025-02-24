6446cbdfe55cd1350090211b12de921370136fdd
Glen Nakamura <glen@imodulo.com>
2025-02-24 12:25:28 -1000

    pcscd winscard-homebrew-1.9.1-2

--- Formula/pcscd.rb
+++ Formula/pcscd.rb
@@ -1,8 +1,8 @@
 class Pcscd < Formula
   desc "Middleware to access a smart card using SCard API"
   homepage "https://pcsclite.apdu.fr"
-  url "https://github.com/glennakamura/PCSC.git", :using => :git, :revision => "3ee428e55b25c981bc1a35c3dc563024e2edd2ce"
-  version "winscard-homebrew-1.9.1"
+  url "https://github.com/glennakamura/PCSC.git", :using => :git, :revision => "720d338e2516d59d6b4a37bc663d9685a84625d7"
+  version "winscard-homebrew-1.9.1-2"
 
   depends_on "autoconf"   => :build
   depends_on "automake"   => :build
