c3e75b4eba284a1212900854b0f0e3544e77b358
Glen Nakamura <glen@imodulo.com>
2025-12-11 06:58:59 -1000

    http-proxy 1.1

--- Formula/http-proxy.rb
+++ Formula/http-proxy.rb
@@ -1,13 +1,13 @@
 class HttpProxy < Formula
   desc "HTTP Proxy"
   homepage "https://github.com/glennakamura/http-proxy"
-  url "https://raw.githubusercontent.com/glennakamura/http-proxy/refs/heads/main/http-proxy-1.0.1.sonoma.bottle.tar.gz"
-  sha256 "9c8d8787c73bc41cd09ed0f8499ede0f406ed0671adf9e3bea1322d29d51aec1"
-  version "1.0.1"
+  url "https://raw.githubusercontent.com/glennakamura/http-proxy/refs/heads/main/http-proxy-1.1.sonoma.bottle.tar.gz"
+  sha256 "230a40bc4422430f57f700dd91b18298255b810afd803739eac9516ab27383bc"
+  version "1.1"
 
   bottle do
     root_url "https://raw.githubusercontent.com/glennakamura/http-proxy/refs/heads/main"
-    sha256 cellar: :any, sonoma: "9c8d8787c73bc41cd09ed0f8499ede0f406ed0671adf9e3bea1322d29d51aec1"
+    sha256 cellar: :any, sonoma: "230a40bc4422430f57f700dd91b18298255b810afd803739eac9516ab27383bc"
   end
 
   def install
