8fbd132e288258caa616b9ff71f5a7f10424613a
Glen Nakamura <glen@imodulo.com>
2025-12-28 12:38:56 -1000

    tlscert 1.3.1

--- Formula/tlscert.rb
+++ Formula/tlscert.rb
@@ -1,13 +1,13 @@
 class Tlscert < Formula
   desc "TLS Certificate Generator"
   homepage "https://github.com/glennakamura/tlscert"
-  url "https://raw.githubusercontent.com/glennakamura/tlscert/refs/heads/main/tlscert-1.3.sonoma.bottle.tar.gz"
-  sha256 "5e5b094e8e5442856ae734f5e02e63fa113aa28fb7bf2ab14010192929d32411"
-  version "1.3"
+  url "https://raw.githubusercontent.com/glennakamura/tlscert/refs/heads/main/tlscert-1.3.1.sonoma.bottle.tar.gz"
+  sha256 "63448be3278f7aad52ff7411dc2bd1c21e6ae180048b3391c8162e3a5378c303"
+  version "1.3.1"
 
   bottle do
     root_url "https://raw.githubusercontent.com/glennakamura/tlscert/refs/heads/main"
-    sha256 cellar: :any, sonoma: "5e5b094e8e5442856ae734f5e02e63fa113aa28fb7bf2ab14010192929d32411"
+    sha256 cellar: :any, sonoma: "63448be3278f7aad52ff7411dc2bd1c21e6ae180048b3391c8162e3a5378c303"
   end
 
   def install
